7cc36ca279270f1a1b6622fa470c18c3e660eaf7
ekohmoney <ekohvictor1151@gmail.com>
2023-07-29 19:58:56 +0100

    first app

---
+++ firstApp.sol
@@ -0,0 +1,10 @@
+// SPDX-Licencse-Identifier:MIT
+pragma solidity ^0.8.17;
+
+contract constants {
+// codeing convertion to uppercase constant variable
+address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+uint public constant MY_UINT = 123;
+
+
+}
